9d4025751c84185a01b2a12310f3331a4cb0c900
madDemian <danielkhorev@gmail.com>
2021-09-02 15:58:08 +0300

    reworked Navbar.js

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -3,28 +3,40 @@ import {Menu} from 'antd';
 import {HomeOutlined, FileTextOutlined, UserSwitchOutlined, UserOutlined, SettingOutlined,} from '@ant-design/icons';
 import Link from "next/link";
 
-function Navbar(){
+function Navbar() {
     return (
-        <div className=' md:w-1/8 lg:w-1/8 fixed font-extrabold pl-64 '>
-            <Menu style={{border:'none'}}>
-                <Menu.Item key="1" icon={<HomeOutlined />}>
-                    <Link href='/'>
-                        <a>Home</a>
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key="2" icon={<FileTextOutlined />}>
-                    Messages
-                </Menu.Item>
-                <Menu.Item key="3" icon={<UserSwitchOutlined />}>
-                    Users
-                </Menu.Item>
-                <Menu.Item key="4" icon={<UserOutlined />}>
-                    Profile
-                </Menu.Item>
-                <Menu.Item key="5" icon={<SettingOutlined />}>
-                    Settings
-                </Menu.Item>
-            </Menu>
+        <div className=' md:w-1/8 lg:w-1/8 fixed font-extrabold pl-64 space-y-2 '>
+            <div className='space-x-2'>
+                <HomeOutlined />
+                <Link href='/'>
+                    <a>Home</a>
+                </Link>
+            </div>
+            <div className='space-x-2'>
+                <FileTextOutlined />
+                <Link href='/'>
+                    <a>Messages</a>
+                </Link>
+            </div>
+            <div className='space-x-2'>
+                <UserSwitchOutlined />
+                <Link href='/'>
+                    <a>Users</a>
+                </Link>
+            </div>
+            <div className='space-x-2'>
+                <UserOutlined />
+                <Link href='/'>
+                    <a>Profile</a>
+                </Link>
+            </div>
+            <div className='space-x-2'>
+                <SettingOutlined />
+                <Link href='/'>
+                    <a>Settings</a>
+                </Link>
+            </div>
+
         </div>
     );
 };
