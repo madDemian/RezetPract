700a11b88818c830ae7c75651e83e12b7bf1596b
madDemian <danielkhorev@gmail.com>
2021-09-06 14:43:16 +0300

    -added validation via yup and formik -fixed HomeBar.js

--- components/Layout/HomeBar.js
+++ components/Layout/HomeBar.js
@@ -1,18 +1,16 @@
 import 'antd/dist/antd.css';
-import {Menu} from 'antd';
 import {HomeOutlined} from '@ant-design/icons';
 import Link from 'next/link'
 
-function HomeBar(){
-    return(
-        <div className='fixed font-extrabold pl-64 '>
-            <Menu style={{border:'none'}}>
-                <Menu.Item key="1" icon={<HomeOutlined />}>
-                    <Link href='/'>
-                        <a>Home</a>
-                    </Link>
-                </Menu.Item>
-            </Menu>
+function HomeBar() {
+    return (
+        <div className='md:w-1/8 lg:w-1/8 fixed font-extrabold pl-64 space-y-2 '>
+            <div className='space-x-2'>
+                <HomeOutlined/>
+                <Link href='/'>
+                    <a>Home</a>
+                </Link>
+            </div>
         </div>
     )
 }
--- components/auth/SignInForm.js
+++ components/auth/SignInForm.js
@@ -1,53 +1,90 @@
-import  classes from '../../styles/sign-in.module.css'
-import {useRouter} from "next/router";
+import classes from '../../styles/sign-in.module.css'
+import {Formik} from "formik"
+import * as yup from 'yup'
+import {useRouter} from "next/router"
 import {useState} from "react";
 
-function SignInForm({onSignIn}){
+function SignInForm({onSignIn}) {
     const router = useRouter()
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
 
-    return(
-        <div>
-            <div className={classes.container}>
-                <div className={classes.title}>
-                    <h1>Sign In</h1>
-                </div>
-                <form onSubmit={(e)=>{
-                    e.preventDefault()
-                    onSignIn({
-                        email,
-                        password
-                    })
-                }}>
-                    <div className={classes.login}>
-                        <label className={classes.inputEmailDescr}>
-                            <p>Username/Email</p>
-                        </label>
-                        <input className={classes.userEmailInput}
-                               placeholder='Email'
-                               type='email'
-                               name='email'
-                               required='required'
-                               onChange={event => setEmail(event.target.value)}
+    const [error, setError] = useState(false)
 
-                        />
-                        <label className={classes.inputPasswordDescr}>
-                            <p>Password</p>
-                        </label>
-                        <input className={classes.userPasswordInput}
-                               placeholder='Password'
-                               type='password'
-                               name='password'
-                               required='required'
-                               onChange={event => setPassword(event.target.value)}
-                        />
-                        <button className={classes.signInButton} type='submit'>Sign In</button>
+    let schema = yup.object({
+        email: yup.string().typeError('Invalid email').required('Required'),
+        password: yup.string().typeError('Invalid password').required('Required')
+    })
 
-                        <p><a className={classes.signUpLink} onClick={()=>(router.push('http://localhost:3000/auth/sign-up'))}>Sign Up</a></p>
-                    </div>
-                </form>
+    return (
+        <div className={classes.container}>
+            <div className={classes.title}>
+                <h1>Sign In</h1>
             </div>
+            <Formik initialValues={{
+                email: '',
+                password: ''
+            }}
+                    validateOnBlur
+                    onSubmit={async(values) => {
+                        setError(await (onSignIn(values)))
+                    }}
+                    validationSchema={schema}
+            >
+                {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit
+                  }) => (
+                    <form onSubmit={handleSubmit}>
+                        {error && <div className='text-center text-red-500'>Invalid credentials</div>}
+                        <div className={classes.login}>
+                            <label className={classes.inputEmailDescr}
+                                   htmlFor={'email'}>
+                                <p>Username/Email</p>
+                            </label>
+                            <input className={classes.userEmailInput}
+                                   placeholder='Email'
+                                   type='email'
+                                   name='email'
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.email}
+                            />
+                            {errors.email && touched.email &&
+                                <div className='text-red-500 font-bold pl-24'>
+                                    {errors.email}
+                                </div>
+
+                            }
+                            <label className={classes.inputPasswordDescr}
+                                   htmlFor='password'>
+                                <p>Password</p>
+                            </label>
+                            <input className={classes.userPasswordInput}
+                                   placeholder='Password'
+                                   type='password'
+                                   name='password'
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.password}
+                            />
+                            {errors.password && touched.password &&
+                                <div className='text-red-500 font-bold pl-24'>
+                                    {errors.password}
+                                </div>
+                            }
+                            <button className={classes.signInButton} type='submit'>Sign In</button>
+                            <p>
+                                <a className={classes.signUpLink}
+                                   onClick={() => (router.push('http://localhost:3000/auth/sign-up'))}>Sign Up
+                                </a>
+                            </p>
+                        </div>
+                    </form>
+                )}
+            </Formik>
         </div>
     )
 }
